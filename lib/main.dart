d83087c1a381554be7d1f5ead077f118be0bbfb8
akterh <akterh42024@gmail.com>
2022-01-23 06:41:18 +0600

    data fetching problem solved

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,9 @@ class _MyAppState extends State<MyApp> {
 
   final List<String> weightList = [];
 
+  final List<String> typeList1 = [];
+  final List<String> typeList2 = [];
+
 
 
 
@@ -36,30 +39,39 @@ class _MyAppState extends State<MyApp> {
         imageList.add(pokemonResponse.sprites.frontShiny);
         heightList.add(pokemonResponse.height.toString());
         weightList.add(pokemonResponse.weight.toString());
+        typeList1.add(pokemonResponse.types[0].type.name.toString());
+        // typeList2.add(pokemonResponse.types[1].type.name.toString());
 
 
       }
-      print(imageList);
+      print(weightList);
+      print(typeList2);
+
 
     }catch(e){
       print(e);
 
     }
 
-    print(weightList);
+    // print(weightList);
+    // print(imageList);
+    print(typeList2);
+    print(typeList1.length);
 
   }
+  Future  futurePokemonData;
 
  @override
   void initState(){
-   pokemonData();
+
    super.initState();
+  futurePokemonData= pokemonData();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp( //use MaterialApp() widget like this
-        home: Home(idList,imageList,nameList,weightList,heightList) //create new widget class for this 'home' to
+        home: Home(idList,imageList,nameList,heightList,weightList,typeList1,typeList2,futurePokemonData) //create new widget class for this 'home' to
       // escape 'No MediaQuery widget found' error
     );
   }
--- lib/pokemon_carousel.dart
+++ lib/pokemon_carousel.dart
@@ -1,86 +1,170 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:test_project_for_apply/repositories/pokemon_repository.dart';
-class Home extends StatelessWidget{
+class Home extends StatefulWidget{
   final  imgList;
   final idList;
   final nameList;
   final weightList;
   final heightList;
-  Home(this.idList,this.imgList,this.nameList,this.heightList,this.weightList);
+  final typeList1;
+  final typeList2;
+  final Future  pokemonData;
+  Home(this.idList,this.imgList,this.nameList,this.heightList,this.weightList,this.typeList1,this.typeList2,this.pokemonData);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentIndex=0;
 
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
+    final height = MediaQuery.of(context).size.height*.4;
     final width = MediaQuery.of(context).size.height*.5;
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Carousel FullScreen Example'),
-        ),
-        body:FutureBuilder(
-          future: PokemonRepository().getPokemonApi(),
-          builder: (context,snapshot){
-            if (snapshot.hasError){
-              print(snapshot.error);
-              print("error");
-              return Container();
-            }  else if(snapshot.hasData){
-              print(snapshot.data);
-              var pokemonApiRespnose = snapshot.data;
-
-              return CarouselSlider.builder(
-                  options: CarouselOptions(
-                    viewportFraction: .3,
-                    autoPlayAnimationDuration: const Duration(milliseconds: 100),
-                    autoPlay: false,
-                    enlargeCenterPage: true,
-                    aspectRatio:2,
-                    height: height,
 
 
 
-                    // pageSnapping: true
-                  ),
-                  itemCount:  pokemonApiRespnose.results.length,
+    return SafeArea(
+      child: Scaffold(
+          body:
+          FutureBuilder(
+            future: widget.pokemonData ,
+            builder: (context,snapshot){
+              if (snapshot.hasError){
+                print(snapshot.error);
+                print("error");
+                return Container();
+              }  else if(snapshot.connectionState==ConnectionState.done){
 
-                  itemBuilder:(BuildContext context, int itemIndex, int pageViewIndex)=>
-                  snapshot.connectionState!=ConnectionState.done?
-                      CircularProgressIndicator(
-                        color: Colors.red,
-                      ):
-                      Container(
-                          child: Stack(
+               print( snapshot.connectionState);
 
-                              children:[
-                                Positioned(child: Card(
-                                  color: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20)
+                return
 
-                                  ),
-                                  elevation: 3,
-                                  shadowColor: Colors.grey,
-                                  child: Container(
-                                    child: Image.network("${imgList[itemIndex]}",),
-                                  ),
-                                )
-                                ),
-                                Positioned(
-                                    child: Text("${idList[itemIndex]}"))
+                  Column(
+                    children: [
+                      CarouselSlider.builder(
+                        options: CarouselOptions(
+                          viewportFraction: .5,
+                          autoPlayAnimationDuration: const Duration(milliseconds: 100),
+                          autoPlay: false,
+                          enlargeCenterPage: true,
+                          aspectRatio:.5,
+                          height: height,
+                          onPageChanged:(i,reason){
+                            var _currentIndex = i;
+                            // var  _currentName = widget.nameList[i];
 
-                              ]
-                            // Text(pokemonApiRespnose.results[itemIndex].url),
+
+                            setState((){
+                              currentIndex=_currentIndex ;
+                              // currentName =_currentName;
+
+
+                            });
+                            // print(currentName);
+                            print(currentIndex);
+                            print(_currentIndex);
+                            // print(widget.typeList1);
+                            // print(widget.typeList2);
+                          },
+
+
+
+                          // pageSnapping: true
+                        ),
+                        itemCount:  widget.imgList.length,
+
+
+                        itemBuilder:(BuildContext context, int itemIndex, int pageViewIndex)=>
+                            Container(
+
+                                child:
+
+
+                                      Card(
+                                        color: Colors.white,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(20)
+
+                                        ),
+                                        elevation: 3,
+                                        shadowColor: Colors.grey,
+                                        child: Container(
+                                          child: Column(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: [
+                                              Image.network("${widget.imgList[itemIndex]}",),
+                                              SizedBox(height: 100,width: 500,),
+
+                                               Text("#${widget.idList[itemIndex]}",style: TextStyle(fontSize: 30),),
+
+                                            ],
+                                          ),
+                                        ),
+                                      )
+
+
+                                          //
+
+
+
+                            )
+
+                ),
+                      SizedBox(height: 60,),
+
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(widget.nameList[currentIndex],style: TextStyle(color: Colors.black,fontSize: 42,fontWeight: FontWeight.bold),)
+                        ],
+                      ) ,
+
+                      SizedBox(height: 60,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+
+                          Padding(
+                            padding: const EdgeInsets.only(left: 16,),
+                            child: Text("Height:${widget.heightList[currentIndex]}",style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold),),
                           ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16,),
+                            child: Text("Weight:${widget.weightList[currentIndex]}",style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold),),
+                          ),
+
+                        ],
+                      ) ,
+                      SizedBox(height: 60,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+
+                          Text("Type:${widget.typeList1[currentIndex]??""}",style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold),)
+                        ],
                       )
+                      // ${widget.typeList1[currentIndex]??""}
+    // ${widget.typeList2[currentIndex]??
 
-              );
-            }else{
-              return Text("error");
-            }
-          },
-        )
+                    ],
+                  );
+              }else{
+                return Center(child: CircularProgressIndicator(
+                  color: Colors.red,
+                ));
+              }
+            },
 
+
+
+          )
+
+      ),
     );
   }
 }
